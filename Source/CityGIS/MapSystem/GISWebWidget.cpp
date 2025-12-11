a10235be17f9d22465e3c62474f48ff18ec1c1ec
Akatoki <akatokikwok@outlook.com>
2025-12-12 01:47:28 +0800

    【Bugfix】:制作左侧滑动列表显示已在地图上绘制好的图元 方便定位和删除.

--- Source/CityGIS/MapSystem/GISWebWidget.cpp
+++ Source/CityGIS/MapSystem/GISWebWidget.cpp
@@ -18,7 +18,20 @@ void UGISWebWidget::NativeConstruct()
 		MapBrowser->LoadURL(Url);
 
 		// 绑定通信
-		MapBrowser->OnUrlChanged.AddDynamic(this, &UGISWebWidget::OnTitleChanged);
+		MapBrowser->OnUrlChanged.AddUniqueDynamic(this, &UGISWebWidget::OnTitleChanged);
+
+		MapBrowser->OnConsoleMessage.AddUniqueDynamic(this, &UGISWebWidget::OnJSMessageRecv);
+	}
+}
+
+void UGISWebWidget::NativeDestruct()
+{
+	Super::NativeDestruct();
+
+	if (MapBrowser)
+	{
+		MapBrowser->OnUrlChanged.RemoveDynamic(this, &UGISWebWidget::OnTitleChanged);
+		MapBrowser->OnConsoleMessage.RemoveDynamic(this, &UGISWebWidget::OnJSMessageRecv);
 	}
 }
 
@@ -37,34 +50,85 @@ void UGISWebWidget::UndoAction()
 	if (MapBrowser) MapBrowser->ExecuteJavascript(TEXT("undoLastPoint();"));
 }
 
-void UGISWebWidget::SaveMap()
+void UGISWebWidget::SaveMapAs(FString FileName)
 {
-	// 触发 JS 导出 -> JS 改 Title -> OnTitleChanged 捕获 -> 写文件
-	if (MapBrowser) MapBrowser->ExecuteJavascript(TEXT("exportData();"));
+	if (!FileName.EndsWith(".json")) FileName += ".json";
+	// 让 JS 导出数据，带上文件名
+	MapBrowser->ExecuteJavascript(FString::Printf(TEXT("exportMap('%s');"), *FileName));
 }
 
 void UGISWebWidget::OnTitleChanged(const FText& TitleText)
 {
-	FString TitleStr = TitleText.ToString();
-	// 拦截协议头 "UE_SAVE:"
-	if (TitleStr.StartsWith("UE_SAVE:"))
+}
+
+void UGISWebWidget::OnJSMessageRecv(const FString& Message, const FString& Source, int32 Line)
+{
+	// Message 就是 JS 里 console.log("...") 的内容
+
+	// 为了防止处理无关的报错日志，我们只处理以 UE_ 开头的
+	if (!Message.StartsWith("UE_")) return;
+
+	// 打印一下，确保我们收到了
+	UE_LOG(LogTemp, Log, TEXT("GIS Received JS Message: %s"), *Message);
+
+	// 1. 处理保存: UE_SAVE:文件名|JSON
+	if (Message.StartsWith("UE_SAVE:"))
 	{
-		FString JsonData = TitleStr.RightChop(8); // 截取后面的 JSON
-		FFileHelper::SaveStringToFile(JsonData, *SaveFilePath);
-		UE_LOG(LogTemp, Warning, TEXT("Map Saved to: %s"), *SaveFilePath);
+		FString Content = Message.RightChop(8);
+		FString Name, Data;
+		if (Content.Split("|", &Name, &Data))
+		{
+			FString Path = FPaths::ProjectSavedDir() + TEXT("GISData/") + Name;
+			FFileHelper::SaveStringToFile(Data, *Path, FFileHelper::EEncodingOptions::ForceUTF8WithoutBOM);
+			UE_LOG(LogTemp, Warning, TEXT("GIS: Saved File to %s"), *Path);
+		}
+	}
+	// 2. 处理添加列表项: UE_ADD:ID|Name
+	else if (Message.StartsWith("UE_ADD:"))
+	{
+		FString Content = Message.RightChop(7);
+		FString ID, Name;
+		// 注意：JS里我们加了时间戳 |Time，这里 Split 只会切第一刀，所以 ID 是干净的，Name 里可能带后缀
+		if (Content.Split("|", &ID, &Name))
+		{
+			// 清理 Name 里的时间戳后缀 (如果有)
+			FString RealName, Trash;
+			if (Name.Split("|", &RealName, &Trash)) Name = RealName;
+
+			if (OnAddPolyItem.IsBound())
+			{
+				OnAddPolyItem.Broadcast(ID, Name);
+			}
+		}
 	}
 }
 
-void UGISWebWidget::LoadMap()
+void UGISWebWidget::LoadMap(FString FileName)
 {
-	FString JsonContent;
-	if (FFileHelper::LoadFileToString(JsonContent, *SaveFilePath))
+	FString Path = FPaths::ProjectSavedDir() + TEXT("GISData/") + FileName;
+	FString Json;
+	if (FFileHelper::LoadFileToString(Json, *Path))
 	{
-		// 清理换行符
-		JsonContent = JsonContent.Replace(TEXT("\n"), TEXT("")).Replace(TEXT("\r"), TEXT(""));
-		FString Script = FString::Printf(TEXT("importData('%s');"), *JsonContent);
-		MapBrowser->ExecuteJavascript(Script);
+		Json = Json.Replace(TEXT("\n"), TEXT("")).Replace(TEXT("\r"), TEXT(""));
+		MapBrowser->ExecuteJavascript(FString::Printf(TEXT("importMap('%s');"), *Json));
 	}
+}
+
+TArray<FString> UGISWebWidget::GetSaveFiles()
+{
+	TArray<FString> Files;
+	IFileManager::Get().FindFiles(Files, *(FPaths::ProjectSavedDir() + TEXT("GISData/*.json")), true, false);
+	return Files;
+}
+
+void UGISWebWidget::FocusID(FString ID)
+{
+	MapBrowser->ExecuteJavascript(FString::Printf(TEXT("focusPoly('%s');"), *ID));
+}
+
+void UGISWebWidget::DeleteID(FString ID)
+{
+	MapBrowser->ExecuteJavascript(FString::Printf(TEXT("deletePoly('%s');"), *ID));
 }
 
 void UGISWebWidget::SearchByName(FString Name)
--- Source/CityGIS/MapSystem/GISWebWidget.h
+++ Source/CityGIS/MapSystem/GISWebWidget.h
@@ -9,35 +9,62 @@
 
 class UWebBrowser;
 
+// 代理：通知蓝图加列表
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnAddPolyItem, FString, ID, FString, Name);
+
 UCLASS()
 class CITYGIS_API UGISWebWidget : public UUserWidget
 {
 	GENERATED_BODY()
 
-public:
-	// 绑定蓝图里的 Web Browser 组件，名字必须叫 MapBrowser
-	UPROPERTY(meta = (BindWidget))
-	UWebBrowser* MapBrowser;
+protected:
+	UPROPERTY(BlueprintAssignable)
+	FOnAddPolyItem OnAddPolyItem;
 
+public:
 	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
 
 	// --- 核心功能接口 (供蓝图调用) ---
 	UFUNCTION(BlueprintCallable)
 	void SetMode(FString ModeName); // "browse", "edit", "snap"
+
 	UFUNCTION(BlueprintCallable)
-	void SaveMap();
+	void SaveMapAs(FString FileName);
+
 	UFUNCTION(BlueprintCallable)
-	void LoadMap();
+	void LoadMap(FString FileName);
+
+	UFUNCTION(BlueprintCallable)
+	TArray<FString> GetSaveFiles();
+
+	UFUNCTION(BlueprintCallable)
+	void FocusID(FString ID);
+	
+	UFUNCTION(BlueprintCallable)
+	void DeleteID(FString ID);
+
 	UFUNCTION(BlueprintCallable)
 	void UndoAction();
+
 	UFUNCTION(BlueprintCallable)
 	void SearchByName(FString Name);
 
 	UFUNCTION(BlueprintCallable)
 	void ActivateReconstructionTool();
 
+	// 绑定蓝图里的 Web Browser 组件，名字必须叫 MapBrowser
+	UPROPERTY(meta = (BindWidget))
+	UWebBrowser* MapBrowser = nullptr;
+
 private:
+	// 监听 JS 发来的数据
 	UFUNCTION()
-	void OnTitleChanged(const FText& TitleText); // 监听 JS 发来的数据
+	void OnTitleChanged(const FText& TitleText);
+
+	//
+	UFUNCTION()
+	void OnJSMessageRecv(const FString& Message, const FString& Source, int32 Line);
+
 	FString SaveFilePath;
 };
